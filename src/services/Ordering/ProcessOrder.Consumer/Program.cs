0ffd64f79ddf09fc7d5ab5769dd1e90969c9bf72
jbw <jbw@users.noreply.github.com>
2020-12-18 13:53:26 +0000

    Update Program.cs

--- src/services/Ordering/ProcessOrder.Consumer/Program.cs
+++ src/services/Ordering/ProcessOrder.Consumer/Program.cs
@@ -1,14 +1,8 @@
 using MassTransit;
 using Microsoft.Extensions.Hosting;
-<<<<<<< HEAD:src/services/Ordering/ProcessOrder.Consumer/Program.cs
 using TooBigToFailBurgerShop.ProcessOrder.Application.Extensions;
 
 namespace TooBigToFailBurgerShop.ProcessOrder.Consumer
-=======
-using TooBigToFailBurgerShop.Ordering.Consumer.Application.Extensions;
-
-namespace TooBigToFailBurgerShop.Ordering.Consumer
->>>>>>> main:src/services/Ordering/Ordering.Consumer/Program.cs
 {
 
     public class Program
@@ -26,5 +20,5 @@ namespace TooBigToFailBurgerShop.Ordering.Consumer
                     services.AddMassTransitHostedService();
                 });
     }
-
+    
 }
