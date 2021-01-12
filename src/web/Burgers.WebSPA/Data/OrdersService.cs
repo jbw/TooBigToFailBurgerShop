ee4b03dbf99eefb4121755990964b18fd70c7189
jbw <jbw@users.noreply.github.com>
2021-01-12 23:26:47 +0000

    support being able to submit an order

--- src/web/Burgers.WebSPA/Data/OrdersService.cs
+++ src/web/Burgers.WebSPA/Data/OrdersService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Burgers.WebSPA.Data
@@ -14,9 +15,20 @@ namespace Burgers.WebSPA.Data
             _httpClient = client;
         }
 
-        public Task<Order> CreateOrder()
-        { 
-            return Task.FromResult(new Order { Name = "Hello" });
+        public async Task CreateOrder()
+        {
+            _httpClient.DefaultRequestHeaders.Remove("x-requestid");
+            _httpClient.DefaultRequestHeaders.Add("x-requestid", Guid.NewGuid().ToString());
+
+            var jsonContent = JsonSerializer.Serialize(new { UserId = "jbw" });
+ 
+            var body = new StringContent(jsonContent, System.Text.Encoding.UTF8, "application/json");
+
+            await _httpClient.PutAsync("/Orders/createorder", body);
+            
         }
     }
+
+
+
 }
