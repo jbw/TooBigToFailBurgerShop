c4ad554ade0428514e66b50ab8c5a8007070fda0
jbw <jbw@users.noreply.github.com>
2021-01-30 22:36:47 +0000

    constrain key to be Guid with Marten implementation

--- src/services/Ordering/Ordering.Persistence.MartenDb/EventsRepository.cs
+++ src/services/Ordering/Ordering.Persistence.MartenDb/EventsRepository.cs
@@ -6,7 +6,7 @@ using TooBigToFailBurgerShop.Ordering.Domain.Core;
 namespace TooBigToFailBurgerShop.Ordering.Persistence.MartenDb
 {
 
-    public class EventsRepository<TType, TKey> : IEventsRepository<TType, TKey> where TType : class, IAggregateRoot<TKey>
+    public class EventsRepository<TType> : IEventsRepository<TType, Guid> where TType : class, IAggregateRoot<Guid>
     {
         private readonly IDocumentStore _store;
 
@@ -19,20 +19,20 @@ namespace TooBigToFailBurgerShop.Ordering.Persistence.MartenDb
         {
             using var session = _store.OpenSession();
 
-            var aggregateRootId = Guid.Parse(aggregateRoot.Id.ToString());
+            var aggregateRootId = aggregateRoot.Id;
             var aggregateVersion = aggregateRoot.Version;
             var events = aggregateRoot.Events;
 
             foreach (var @event in events)
             {
 
-                session.Events.Append(aggregateRootId, aggregateRoot, @event);
+                session.Events.Append(aggregateRootId, aggregateVersion, @event);
             }
 
             await session.SaveChangesAsync();
         }
 
-        public Task<TType> RehydrateAsync(TKey key)
+        public Task<TType> RehydrateAsync(Guid key)
         {
             throw new NotImplementedException();
         }
