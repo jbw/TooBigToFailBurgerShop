a1ab487a889aa5cd650f9dca264d54a4da42021f
jbw <jbw@users.noreply.github.com>
2021-01-30 23:12:20 +0000

    add rabbitmq producer

--- src/services/Ordering/Ordering.Persistence.RabbitMQ/EventProducer.cs
+++ src/services/Ordering/Ordering.Persistence.RabbitMQ/EventProducer.cs
@@ -1,15 +1,46 @@
-﻿using System;
+﻿using MassTransit;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using TooBigToFailBurgerShop.Ordering.Domain.Core;
 using TooBigToFailBurgerShop.Ordering.Domain.Core.EventBus;
 
 namespace TooBigToFailBurgerShop.Ordering.Persistence.RabbitMQ
 {
+    public class Envelope
+    {
+        public Guid Key { get; set; }
+        public string Value { get; set; }
+    }
+
     public class EventProducer<TType, TKey> : IEventProducer<TType, TKey> where TType : IAggregateRoot<TKey>
     {
-        public Task DispatchAsync(TType aggregateKey)
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public EventProducer(IPublishEndpoint publishEndpoint)
         {
-            throw new NotImplementedException();
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public async Task DispatchAsync(TType aggregateRoot)
+        {
+            if (null == aggregateRoot)
+                throw new ArgumentNullException(nameof(aggregateRoot));
+
+            if (!aggregateRoot.Events.Any())
+                return;
+
+            foreach (var @event in aggregateRoot.Events)
+            {
+                var data = System.Text.Json.JsonSerializer.Serialize(@event);
+
+                // Configure endpoint
+                await _publishEndpoint.Publish<Envelope>(new
+                {
+                    Key = @event.AggregateId,
+                    Value = data,
+                });
+            }
         }
     }
 }
---
+++ src/services/Ordering/Ordering.Persistence.RabbitMQ/IServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TooBigToFailBurgerShop.Ordering.Persistence.MartenDb
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRabbitMqInfrastructure(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
