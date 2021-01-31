7e61c0b481c01ba8535fa427e09701689db25af6
jbw <jbw@users.noreply.github.com>
2021-01-31 22:11:00 +0000

    Update CreateOrderCommandHandler.cs

--- src/services/Ordering/Ordering.API/Application/Order/CreateOrderCommandHandler.cs
+++ src/services/Ordering/Ordering.API/Application/Order/CreateOrderCommandHandler.cs
@@ -1,23 +1,31 @@
 ﻿using MassTransit;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using TooBigToFailBurgerShop.Ordering.Domain;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 using TooBigToFailBurgerShop.Infrastructure.Idempotency;
 using TooBigToFailBurgerShop.Ordering.Contracts;
+using TooBigToFailBurgerShop.Ordering.Domain.Core;
 
 namespace TooBigToFailBurgerShop.Application.Commands.Order
 {
+    using Order = Ordering.Domain.AggregatesModel.Order;
+
     public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, bool>
     {
         private readonly IPublishEndpoint _publishEndpoint;
         private readonly ILogger<CreateOrderCommand> _logger;
+        private readonly IEventsService<Order, Guid> _orderEventsService;
+        private readonly IOrdersRepository _orderRepository;
 
-        public CreateOrderCommandHandler(IPublishEndpoint publishEndpoint, ILogger<CreateOrderCommand> logger)
+        public CreateOrderCommandHandler(IPublishEndpoint publishEndpoint, IOrdersRepository ordersRepository, IEventsService<Order, Guid> orderEventsService, ILogger<CreateOrderCommand> logger)
         {
             _publishEndpoint = publishEndpoint;
             _logger = logger;
+            _orderEventsService = orderEventsService;
+            _orderRepository = ordersRepository;
         }
 
         /// <summary>
@@ -38,8 +46,12 @@ namespace TooBigToFailBurgerShop.Application.Commands.Order
 
             };
 
-            // Submit the burger order as a message and we're done. 
-            await _publishEndpoint.Publish<SubmitBurgerOrder>(message, cancellationToken);
+            // Consistency solution: 
+            // We've moved the create order code into a consumer which runs in the saga
+            // scope. This give us transactional behaviour. 
+            // Limitation: This publish could still fail and not get handled but we 
+            // don't run into consistency issues. 
+            await _publishEndpoint.Publish<CreateBurgerOrder>(message, cancellationToken);
 
             return true;
         }
