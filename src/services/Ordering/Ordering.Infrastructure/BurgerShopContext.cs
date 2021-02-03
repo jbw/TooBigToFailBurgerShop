16018437e5fc5b972e95e2d28387db1b8fa78414
jbw <jbw@users.noreply.github.com>
2021-02-03 22:41:45 +0000

    add context to infrastructure project

---
+++ src/services/Ordering/Ordering.Infrastructure/BurgerShopContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Logging;
+using TooBigToFailBurgerShop.Ordering.Infrastructure.EntityConfigurations;
+
+namespace TooBigToFailBurgerShop.Ordering.Infrastructure
+{
+    public class BurgerShopContext: DbContext
+    {
+        public BurgerShopContext() { }
+
+        public BurgerShopContext(DbContextOptions<BurgerShopContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BurgerOrderEntityTypeConfiguration());
+        }
+    }
+
+    public class BurgerShopContextFactory : IDesignTimeDbContextFactory<BurgerShopContext>
+    {
+
+        public BurgerShopContext CreateDbContext(string[] args)
+        {
+  
+            var connectionSting = "host=localhost;database=burgers;user id=burger;password=burger";
+
+            var optionsBuilder = new DbContextOptionsBuilder<BurgerShopContext>();
+          
+            optionsBuilder.UseNpgsql(connectionSting);
+
+            return new BurgerShopContext(optionsBuilder.Options);
+        }
+    }
+
+}
