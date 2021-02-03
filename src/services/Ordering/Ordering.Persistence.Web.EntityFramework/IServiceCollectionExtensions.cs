46d854d78bc958123c22f6d9df7e6c76715301c7
jbw <jbw@users.noreply.github.com>
2021-02-03 22:41:19 +0000

    add orders repo implementation

---
+++ src/services/Ordering/Ordering.Persistence.Web.EntityFramework/IServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TooBigToFailBurgerShop.Ordering.Domain;
+
+namespace TooBigToFailBurgerShop.Ordering.Persistence.Web.EntityFramework
+{
+    public class AddEntityFrameworkOrderingPeristanceConfigurator
+    {
+
+    }
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEntityFrameworkOrderRepository<TContext, TType>(this IServiceCollection services) where TContext : DbContext where TType : class
+        {
+
+            services.AddSingleton<IOrdersRepository, OrdersRepository<TContext, TType>>();
+
+            return services;
+        }
+    }
+}
--- src/services/Ordering/Ordering.Persistence.Web.EntityFramework/OrdersRepository.cs
+++ src/services/Ordering/Ordering.Persistence.Web.EntityFramework/OrdersRepository.cs
@@ -1,14 +1,24 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Threading.Tasks;
 using TooBigToFailBurgerShop.Ordering.Domain;
+using TooBigToFailBurgerShop.Ordering.Domain.AggregatesModel;
 
 namespace TooBigToFailBurgerShop.Ordering.Persistence.Web.EntityFramework
 {
-    public class OrdersRepository : IOrdersRepository
+    public class OrdersRepository<TContext, TType> : IOrdersRepository where TContext : DbContext where TType : class
     {
-        public Task CreateAsync(Guid orderId)
+        private TContext _context;
+
+        public OrdersRepository(TContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+
+        public async Task CreateAsync(Guid orderId)
+        {
+            await _context.AddAsync(new Order(orderId));
+            await _context.SaveChangesAsync();
         }
     }
 }
