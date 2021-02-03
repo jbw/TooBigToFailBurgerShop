c708c4b38692c6a4b889125372721af377bef8de
jbw <jbw@users.noreply.github.com>
2021-02-03 22:39:04 +0000

    add infrastructure project

---
+++ src/services/Ordering/Ordering.Infrastructure/EntityConfigurations/BurgerOrderEntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TooBigToFailBurgerShop.Ordering.Domain.AggregatesModel;
+
+namespace TooBigToFailBurgerShop.Ordering.Infrastructure.EntityConfigurations
+{
+    public class BurgerOrderEntityTypeConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(o => o.Id);
+
+            // we should ignore domain events
+            builder.Ignore(b => b.Events);
+
+        }
+    }
+}
---
+++ src/services/Ordering/Ordering.Infrastructure/Idempotency/RequestManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+using TooBigToFailBurgerShop.Ordering.Infrastructure;
+
+namespace TooBigToFailBurgerShop.Ordering.Infrastructure.Idempotency
+{
+    public class RequestManager : IRequestManager
+    {
+        public RequestManager(BurgerShopContext context, ILogger<RequestManager> logger)
+        {
+
+        }
+    }
+}
