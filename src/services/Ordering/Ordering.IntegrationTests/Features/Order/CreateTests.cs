4da78c45c276408a352e9612cec692324fcf80ad
Jason Watson <hi@jbw.codes>
2021-02-16 22:15:34 +0000

    Integration tests (#24)
    * initial project setup
    
    * Create CreateTests.cs
    
    * create order http test
    
    * add coverlet.msbuild package
    
    Co-authored-by: jbw <jbw@users.noreply.github.com>

---
+++ src/services/Ordering/Ordering.IntegrationTests/Features/Order/CreateTests.cs
@@ -0,0 +1,55 @@
+﻿using MassTransit.Testing;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Shouldly;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Ordering.IntegrationTests.Features.Order
+{
+
+    public class OrderApiTests : IClassFixture<OrderWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+        private readonly OrderWebApplicationFactory _factory;
+
+        public OrderApiTests(OrderWebApplicationFactory factory, ITestOutputHelper outputHelper)
+        {
+            _factory = factory; 
+            _factory.OutputHelper = outputHelper;
+
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact]
+        public async Task Should_create_new_order()
+        {
+            var harness = new InMemoryTestHarness();
+
+            await harness.Start();
+
+            try
+            {
+                // Given
+                var url = "/Orders/createorder";
+                var orderContent = JsonContent.Create(new { });
+
+                // When
+                _client.DefaultRequestHeaders.Add("x-requestid", "3fa85f64-5717-4562-b3fc-2c963f66afa6");
+                var resp = await _client.PutAsync(url, orderContent);
+
+                // Then
+                resp.EnsureSuccessStatusCode();
+            }
+            finally
+            {
+                await harness.Stop();
+            }
+        }
+    }
+}
