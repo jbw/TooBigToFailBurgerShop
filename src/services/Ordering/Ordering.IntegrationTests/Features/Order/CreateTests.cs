669b0e25907e9a4bc576d6e10fc08c4325c1e2fb
jbw <jbw@users.noreply.github.com>
2021-02-16 20:02:51 +0000

    Create CreateTests.cs

---
+++ src/services/Ordering/Ordering.IntegrationTests/Features/Order/CreateTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ordering.IntegrationTests.Features.Order
+{
+    public class CreateTests
+    {
+        [Fact]
+        public async Task Should_create_new_order()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
