7681e06af581fa948ad9e8b66efe485a3a9a71d8
jbw <hi@jbw.codes>
2021-04-09 23:13:15 +0100

    clean up

--- src/services/Ordering/ProcessOrder.Consumer/Program.cs
+++ src/services/Ordering/ProcessOrder.Consumer/Program.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenTelemetry;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using System.Diagnostics;
