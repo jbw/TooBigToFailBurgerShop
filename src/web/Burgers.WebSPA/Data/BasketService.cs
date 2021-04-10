2a5c88f7bc2f1a4a1d8af41e03910fdd177bd896
jbw <hi@jbw.codes>
2021-04-10 20:42:40 +0100

    add basket api using darp

---
+++ src/web/Burgers.WebSPA/Data/BasketService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+namespace Burgers.WebSPA.Data
+{
+    public class BasketService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BasketService(HttpClient client)
+        {
+            _httpClient = client;
+        }
+
+        public async Task AddItemToCustomerBasket()
+        {
+            _httpClient.DefaultRequestHeaders.Remove("x-requestid");
+            _httpClient.DefaultRequestHeaders.Add("x-requestid", Guid.NewGuid().ToString());
+
+
+            var customerId = Guid.NewGuid().ToString();
+            var items = new List<string> { };
+
+            var body = new StringContent(
+                JsonSerializer.Serialize(new { customerId, items }), 
+                System.Text.Encoding.UTF8, 
+                "application/json"
+            );
+
+            await _httpClient.PostAsync("/Basket", body);
+
+        }
+    }
+
+}
