76969a601e663487b61c9884909a88d4390b0066
jbw <hi@jbw.codes>
2021-05-11 19:17:19 +0100

    update api tests

--- src/services/Ordering/Ordering.IntegrationTests/Features/Order/CreateTests.cs
+++ src/services/Ordering/Ordering.IntegrationTests/Features/Order/CreateTests.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using JohnKnoop.MongoRepository;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using TooBigToFailBurgerShop;
+using TooBigToFailBurgerShop.Ordering.Application.Queries.Models;
 using TooBigToFailBurgerShop.Ordering.Persistence.Mongo;
 using Xunit;
 using Xunit.Abstractions;
@@ -11,20 +17,25 @@ using Xunit.Abstractions;
 namespace Ordering.IntegrationTests.Features.Order
 {
 
-    public class OrderApiTests : IClassFixture<OrderWebApplicationFactory>
+    public class OrderApiTests 
     {
+        private readonly IConfiguration _configuration;
         private readonly HttpClient _client;
-        private readonly OrderWebApplicationFactory _factory;
 
-        public OrderApiTests(OrderWebApplicationFactory factory, ITestOutputHelper outputHelper)
+        public static IConfiguration GetConfiguration()
         {
-            _factory = factory;
-            _factory.OutputHelper = outputHelper;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
 
-            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
-            {
-                AllowAutoRedirect = false
-            });
+            return builder.Build();
+        }
+
+        public OrderApiTests()
+        {
+            _configuration = GetConfiguration();
+            _client = new HttpClient();
         }
 
         [Fact]
@@ -32,7 +43,7 @@ namespace Ordering.IntegrationTests.Features.Order
         {
 
             // Given
-            var url = "/Orders/createorder";
+            var url = "http://localhost:16969/Orders/createorder";
             var orderContent = JsonContent.Create(new { });
 
             // When
@@ -48,13 +59,33 @@ namespace Ordering.IntegrationTests.Features.Order
         public async Task Should_get_order_by_id()
         {
             // Given
-            IMongoClient mongoClient = (IMongoClient)_factory.Services.GetService(typeof(IMongoClient));
+            var options = _configuration
+                           .GetSection(typeof(OrderIdRepositorySettings).Name)
+                           .Get<OrderIdRepositorySettings>()
+                           .Connection;
+
+            var mongoClientSettings = new MongoClientSettings
+            {
+                Credential = MongoCredential.CreateCredential(options.Database, options.Username, options.Password),
+                Server = new MongoServerAddress(options.Host, (int)options.Port),
+                Scheme = ConnectionStringScheme.MongoDB
+            };
+
+            IMongoClient mongoClient = new MongoClient(mongoClientSettings);
+
+            MongoRepository.Configure()
+                .Database(options.Database, db => db
+                    .MapAlongWithSubclassesInSameAssebmly<OrderId>(options.CollectionName)
+                    .MapAlongWithSubclassesInSameAssebmly<OrderArchiveItem>())
+                .AutoEnlistWithTransactionScopes()
+                .Build();
+
             var repo = new OrdersArchiveItemRepository(mongoClient);
             var id = Guid.NewGuid();
             await repo.CreateAsync(id, DateTime.UtcNow);
 
             // When
-            var getOrderByIdUrl = "/Orders/getorder?id=" + id;
+            var getOrderByIdUrl = "http://localhost:16969/Orders/getorder?id=" + id;
             _client.DefaultRequestHeaders.Add("x-requestid", "3fa85f64-5717-4562-b3fc-2c963f66afa6");
             var resp = await _client.GetAsync(getOrderByIdUrl);
 
@@ -66,7 +97,7 @@ namespace Ordering.IntegrationTests.Features.Order
         public async Task Should_get_all_orders()
         {
             // Given
-            var url = "/Orders/getorders";
+            var url = "http://localhost:16969/Orders/getorders";
 
             // When
             _client.DefaultRequestHeaders.Add("x-requestid", "3fa85f64-5717-4562-b3fc-2c963f66afa6");
